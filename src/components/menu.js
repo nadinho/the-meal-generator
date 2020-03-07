c7cd2c3b59cce34836f2828048e9db1537c88700
nadinho <rockt2935@gmail.com>
2020-03-07 19:14:33 +0100

    fetch api

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,8 +1,19 @@
 import { createElement } from '../lib/dom';
 
-export function createMenu() {
+async function fetchMenu() {
+  const response = await fetch(
+    'https://www.themealdb.com/api/json/v1/1/random.php'
+  );
+  const results = await response.json();
+  const recipe = results.meals[0];
+
+  console.log(recipe);
+}
+
+export function fetchDataCreateMenu() {
   const menuSection = createElement('section', {
     className: 'menu'
   });
+  fetchMenu();
   return menuSection;
 }
